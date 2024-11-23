de703afc551b9c7f025fc3e949d257b95f45fc03
selam <hoigjgrxkj@gmail.com>
2024-11-23 10:31:52 -0800

    Initial commit

---
+++ menustrip/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace menustrip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setColorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rcbx = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setColorsToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // setColorsToolStripMenuItem
+            // 
+            this.setColorsToolStripMenuItem.Name = "setColorsToolStripMenuItem";
+            this.setColorsToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.setColorsToolStripMenuItem.Text = "Set Colors";
+            this.setColorsToolStripMenuItem.Click += new System.EventHandler(this.setColorsToolStripMenuItem_Click);
+            // 
+            // rcbx
+            // 
+            this.rcbx.BackColor = System.Drawing.Color.White;
+            this.rcbx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rcbx.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rcbx.Font = new System.Drawing.Font("Cascadia Code", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rcbx.Location = new System.Drawing.Point(0, 24);
+            this.rcbx.Name = "rcbx";
+            this.rcbx.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.rcbx.Size = new System.Drawing.Size(800, 426);
+            this.rcbx.TabIndex = 1;
+            this.rcbx.Text = "SELAM AHMED";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rcbx);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Note";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setColorsToolStripMenuItem;
+        public System.Windows.Forms.RichTextBox rcbx;
+    }
+}
+
---
+++ menustrip/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace menustrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void setColorsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+         
+            using (Form2 form = new Form2())
+            {
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    rcbx.BackColor = form.SelectedBackColor;
+                    rcbx.ForeColor = form.SelectedFontColor;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+            rcbx.BackColor = Color.White;
+            rcbx.ForeColor = Color.Black;
+        }
+    }
+}
